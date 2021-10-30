9e77d2b09e6347d053b1d427344b38812fedd1c1
Futsum Nemariam <futsumnemariam2@gmil.com>
2021-10-30 14:06:16 -0700

    initial commit

--- src/components/Singup.js
+++ src/components/Singup.js
@@ -5,15 +5,15 @@ const Singup = () => {
     return (
         <div>
             <section class="vh-100" style={{backgroundColor:  "#508bfc"}}>
-  <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100" style={{borderRadius: "1rem"}}>
       <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style={{borderRadius: "25px"}}>
+        <div class="">
           <div class="card-body p-md-5 text-center">
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                <h3 class="mb-5">Sign up</h3>
 
                 <form class="mx-1 mx-md-4">
 
@@ -38,7 +38,7 @@ const Singup = () => {
                     <div class="form-outline flex-fill mb-0">
                       <label class="form-label" for="form3Example3c">Write your Phone</label>
                       <input type="email" id="form3Example3c" class="form-control" placeholder="enter your phone"/>
-                    </div>
+                     </div>
                   </div>
 
                   <div class="d-flex flex-row align-items-center mb-4">
